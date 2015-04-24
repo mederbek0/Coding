f6a8b2c08ed660882fabf9eeefd51c25361d0cef
mederbek0 <mederbek0@gmail.com>
2015-04-24 16:20:17 -0500

    update

--- m/src/m/M.java
+++ m/src/m/M.java
@@ -1,8 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+MeregeSort
+ * */
 
 package m;
 
@@ -10,13 +8,49 @@ package m;
  *
  * @author mederbekbegmatov
  */
+import java.util.Random;
+import java.util.Scanner;
 public class M {
 
     /**
      * @param args the command line arguments
      */
+    //
+    public static class Ar
+    {
+        int[] a,b;
+        int length;
+        
+        public Ar(int n)
+        {
+            a=new int[n];
+            b=new int[n];
+            length=n;
+        }
+        public void print()
+        {
+            for(int i=0;i<length;i++)
+                System.out.print(a[i]+" ");
+        }
+        
+        public void input(int j)
+        {
+            Random ran=new Random();
+            for (int i=0;i<length;i++)
+            {
+                a[i]=ran.nextInt()%j;
+            }
+        }
+        
+        
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner in= new Scanner(System.in);
+        int n=in.nextInt();
+        Ar a=new Ar(n);
+        a.input(100);
+        a.print();
     }
     
 }
