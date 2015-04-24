044104a9b4c9a0c114dc3cfd8e61136dff656914
mederbek0 <mederbek0@gmail.com>
2015-04-24 17:11:24 -0500

    Merge sort

--- m/src/m/M.java
+++ m/src/m/M.java
@@ -42,6 +42,54 @@ public class M {
             }
         }
         
+        //MeregeSort function
+        public void msort(int left,int right)
+        {
+            if (left<right)
+            {
+                int mid=(left+right)/2;
+                
+                msort(left,mid);
+                msort(mid+1,right);
+                merge(left,mid,right);
+            }
+        }
+        public void merge(int left, int mid,int right)
+        {
+            for (int i=left;i<=right;i++)
+                b[i]=a[i];
+            int i=left;
+            int j=left,k=mid+1;
+            while (j<=mid && k<=right)
+            {
+                if (b[j]<=b[k])
+                {
+                  
+                    a[i]=b[j];
+                    j++;
+                  
+                }
+                else
+                {
+                    a[i]=b[k];
+                    k++;
+                }       
+                i++;
+                
+            }
+            while (k<=right)
+            {
+                a[i]=b[k];
+                i++;k++;
+            }
+            while (j<=mid)
+            {
+                a[i]=b[j];
+                i++;j++;
+            }
+            
+        }
+        
         
     }
     public static void main(String[] args) {
@@ -50,6 +98,9 @@ public class M {
         int n=in.nextInt();
         Ar a=new Ar(n);
         a.input(100);
+        a.print();
+        a.msort(0,n-1);
+        System.out.println();
         a.print();
     }
     
