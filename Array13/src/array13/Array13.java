9444ad821b5021f5e5c6edf723cee3ab097fd420
mederbek0 <mederbek0@gmail.com>
2015-05-03 23:28:22 -0500

    Sort numbers by frequencies

--- Array13/src/array13/Array13.java
+++ Array13/src/array13/Array13.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Write a program to print all the LEADERS in the array. An element is 
+leader if it is greater than all the elements to its right side. And the 
+rightmost element is always a leader.
+For example int the array {16, 17, 4, 3, 5, 2}, leaders are 17, 5 and 2.
  */
 
 package array13;
