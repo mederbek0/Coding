248fafb9708f617f1e070c3c270864763a446336
mederbek0 <mederbek0@gmail.com>
2015-05-10 16:34:11 -0500

    print out largest (or smallest) k numbers in an array

---
+++ Array20/src/array20/Array20.java
@@ -0,0 +1,21 @@
+/*
+Write an efficient program for printing k largest elements in an array. 
+Elements in array can be in any order.
+ */
+
+package array20;
+
+/**
+ *
+ * @author mederbekbegmatov
+ */
+public class Array20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
