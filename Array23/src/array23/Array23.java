2c3b7d3ea60544ff5824db9741b8d857f1d0d691
mederbek0 <mederbek0@gmail.com>
2015-05-15 18:04:57 -0500

    update

--- Array23/src/array23/Array23.java
+++ Array23/src/array23/Array23.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Given a sorted array and a value x, the ceiling of x is the smallest 
+element in array greater than or equal to x, and the floor is the greatest 
+element smaller than or equal to x. Assume than the array is sorted in non-decreasing order. 
+Write efficient functions to find floor and ceiling of x.
  */
 
 package array23;
