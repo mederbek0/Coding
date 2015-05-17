eb05b76ea707962c55729517917be10e04a13751
mederbek0 <mederbek0@gmail.com>
2015-05-17 08:11:44 -0500

    Sort 0,1 and 2

---
+++ Array27/src/array27/Array27.java
@@ -0,0 +1,24 @@
+/*
+ Given an array A[] consisting 0s, 1s and 2s, write 
+a function that sorts A[]. The functions should put all 0s first, 
+then all 1s and all 2s in last.
+
+
+ */
+
+package array27;
+
+/**
+ *
+ * @author mederbekbegmatov
+ */
+public class Array27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
