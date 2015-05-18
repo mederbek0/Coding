88929d3bbaa55086392771238e2c99c25f957844
mederbek0 <mederbek0@gmail.com>
2015-05-18 15:59:51 -0500

    update

--- Array27/src/array27/Array27.java
+++ Array27/src/array27/Array27.java
@@ -8,6 +8,8 @@ then all 1s and all 2s in last.
 
 package array27;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mederbekbegmatov
@@ -17,6 +19,52 @@ public class Array27 {
     /**
      * @param args the command line arguments
      */
+    public static class Ar
+    {
+          int[] a,b;
+        int length;
+        public Ar(int n)
+        {
+            a= new int[n];
+            b=new int[n];
+            length=n;
+            for (int i=0;i<n-2;i++)
+                b[i]=0;
+     
+        }
+         public void input()
+        {
+            Scanner in =new Scanner(System.in);
+            for (int i=0;i<length;i++)
+            a[i]=(in.nextInt()%100);
+        }
+         
+         public int abs(int b)
+         {
+             if (b>0)
+                 return b;
+             else 
+                 return b*(-1);
+                         
+         }
+       
+        public void print()
+        {
+            for (int i=0;i<length;i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+        }
+        public void swap(int k,int l)
+        {
+            int temp=a[k];
+            a[k]=a[l];
+            a[l]=temp;
+        }
+        
+        
+     
+      
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
