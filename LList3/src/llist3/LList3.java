70f9f3110b861a2e0099f3add1076810b2be6d7f
mederbek0 <mederbek0@gmail.com>
2015-08-01 20:51:55 -0400

    List segregation

---
+++ LList3/src/llist3/LList3.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package llist3;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.File;
+/**
+ *
+ * @author mederbekbegmatov
+ */
+public class LList3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException{
+        // TODO code application logic here
+        Scanner in =new Scanner(new File("input.txt"));
+        LinkedList list=new LinkedList();
+        while(in.hasNext())
+        {
+            list.addNode(new Node(in.nextInt()));
+        }
+        list.print();
+        Node a=new Node(4);
+        Node b=new Node(3);
+        list.addNode(a);
+        list.addNodeToEnd(b);
+        list.print();
+        list.remove(b);
+        list.print();
+        list.segregate();
+        list.print();
+    }
+    
+}
---
+++ LList3/src/llist3/LinkedList.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package llist3;
+
+/**
+ *
+ * @author mederbekbegmatov
+ */
+public class LinkedList {
+    Node head;
+    public LinkedList(){
+        this.head=null;
+    }
+    //
+    public void addNode(Node a){
+        if (this.head==null) 
+            this.head=a;
+        else
+        {
+            a.next=this.head;
+            this.head=a;
+        }
+    
+    }
+    //
+    public void addNodeToEnd(Node a){
+        Node first=this.head;
+        if(head==null){
+            head=a;
+        }
+        else
+        {
+            while(first.next!=null)
+            {
+                first=first.next;
+            }
+            first.next=a;
+        }
+    
+    }
+    //
+    public void remove(Node k){
+        Node first=this.head;
+        Node prev=null;
+        while (first!=k){
+            prev=first;
+            first=first.next;
+        }
+        if (prev==null){
+           head=head.next; 
+        }else
+        {
+            if (first==k)
+            {
+                prev.next=first.next;
+            }
+            else
+            {
+                System.out.println("couldn't find Node");
+            }
+        }
+    }
+    
+    //
+    
+    public void segregate(){
+        Node first=this.head;
+        Node prev=null;
+        Node prev1=null;
+        Node temp=null;
+        while(first!=null){
+            if (first.data%2==1)
+            {
+                temp=first;
+                first=first.next;
+                remove(temp);
+                if(prev==null){
+                    temp.next=null;
+                    prev=temp;
+                    prev1=temp;
+                }else
+                {
+                    temp.next=null;
+                    prev.next=temp;
+                    
+                    prev=prev.next;
+                }
+               // System.out.print(temp.data+" + ");
+            }
+            else
+            {
+                first=first.next;
+            }
+        
+        }
+        first=head;
+        prev=null;
+        while(first!=null){
+            prev=first;
+            first=first.next;}
+        prev.next=prev1;
+        
+        
+    }
+    
+    //
+    public void print(){
+        Node first=this.head;
+        while(first!=null){
+            System.out.print(first.data+" ");
+            first=first.next;
+        }
+        System.out.println(); 
+    }
+    
+    
+    
+}
