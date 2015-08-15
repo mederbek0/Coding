fd36b4ad9345c0b2d56d8d31ebb9a062c095a8f7
mederbek0 <mederbek0@gmail.com>
2015-08-14 22:57:45 -0400

    update

--- Tree1/src/tree1/Tree1.java
+++ Tree1/src/tree1/Tree1.java
@@ -20,18 +20,25 @@ public class Tree1 {
         // TODO code application logic here
         Scanner in=new Scanner(new File("input.txt"));
         Tree tr=new Tree();
-       // System.out.println("1");
-        int k;
-        while(in.hasNext())
-        {
-            k=in.nextInt();
-            tr.add(new Node(k));
+        Tree tr1=new Tree();
+        // System.out.println("1");
+        tr.root=new Node(1);
+        tr1.root=new Node(1);
+        tr.root.right=new Node(4);
+        tr1.root.right=new Node(4);
+        tr.root.left= new Node(3);
+        tr1.root.left= new Node(3);
+        tr.root.left.right= new Node(5);
+        tr1.root.left.right= new Node(5);
+        tr.root.left.left= new Node(2);
+        tr1.root.left.left= new Node(2);
         
-        }
         tr.traverse(tr.root);
      //  System.out.println(tr.root.data);
        
         System.out.println(tr.sizeOf(tr.root));
     }
+    
+    
     
 }
